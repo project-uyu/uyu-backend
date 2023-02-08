8b3580d0e3de4c5d80ac77dd6a08a6eeeca7246f
algosipdahack <sophia5460@ewhain.net>
2023-02-08 18:39:19 +0900

    [feat] Controller 기본틀 설정

--- src/main/java/uyu/server/tag/web/TagApiController.java
+++ src/main/java/uyu/server/tag/web/TagApiController.java
@@ -1,4 +1,32 @@
 package uyu.server.tag.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tags/")
+@RequiredArgsConstructor
 public class TagApiController {
+    @PostMapping("new")
+    public ResponseEntity<Long> createTag(@RequestParam(value = "name") String name) {
+        return ResponseEntity.status(HttpStatus.CREATED).body();
+    }
+
+    @GetMapping("list")
+    public ResponseEntity<> getTagList() {
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
+
+    @PatchMapping("{tagId}")
+    public ResponseEntity<Long> modifyTag(@RequestParam(value = "name") String name) {
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
+
+    @DeleteMapping("{tagId}")
+    public ResponseEntity<Long> deleteTag() {
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
 }
