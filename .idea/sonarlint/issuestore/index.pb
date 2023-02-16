b5bc03aaf5f31725e664dba53d73d95ffc291768
algosipdahack <sophia5460@ewhain.net>
2023-02-16 10:52:39 +0900

    [feat] 서비스 테스트 구성 중

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+a
+1src/main/java/uyu/server/util/BaseTimeEntity.java,8/9/892fa61bf598b9ba79c503de60a215027ce8cdb6
+7
+gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+f
+6src/main/java/uyu/server/tag/web/TagApiController.java,e/1/e13e313b54d8b2acc694763b818b002b60c58518
+o
+?src/main/java/uyu/server/link/service/impl/LinkServiceImpl.java,a/a/aa07c02809c6d25bae39784c8a0b8cc7337ffeef
+u
+Esrc/main/java/uyu/server/tag/repository/repository/TagRepository.java,9/7/97433c00723a8f5ce0ac70fb0d6dc42fc1c2fb94
--- src/test/java/uyu/server/link/service/LinkServiceTest.java
+++ src/test/java/uyu/server/link/service/LinkServiceTest.java
@@ -1,4 +1,37 @@
 package uyu.server.link.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uyu.server.link.service.impl.LinkServiceImpl;
+import uyu.server.link.web.dto.LinkRequestDto;
+import uyu.server.link.web.dto.LinkResponseDto;
+import uyu.server.tag.web.dto.TagListResponseDto;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
 public class LinkServiceTest {
+    @Mock
+    private LinkServiceImpl linkService;
+    LinkResponseDto responseDto;
+    LinkRequestDto requestDto;
+    List<TagListResponseDto> dtoList;
+    @BeforeEach
+    void settings() {
+        dtoList = new LinkedList<>();
+        dtoList.add(new TagListResponseDto(1L,"태그 이름"));
+        responseDto = LinkResponseDto.builder()
+                .hit(1L)
+                .modifiedDate(LocalDate.now())
+                .createdDate(LocalDate.now())
+                .content("링크 내용")
+                .url("https://github.com/project-uyu/uyu-backend")
+                .tagLists(dtoList)
+                .build();
+        requestDto = new LinkRequestDto("https://github.com/project-uyu/uyu-backend","링크 내용",dtoList);
+    }
 }
