0314ddfc437eb3d42421786791d98219307763e8
Lani Jeong <71077419+ye1ne@users.noreply.github.com>
2023-02-24 22:12:37 +0900

    [feat] member entity 만들기

---
+++ src/main/java/uyu/server/member/data/entity/Member.java
@@ -0,0 +1,30 @@
+package uyu.server.member.data.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "member")
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String password;
+
+    private String name;
+
+    @Builder
+    public Member(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
