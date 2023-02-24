0fa13ab55d8fd5e1658f57af6561666767fc16b6
Lani Jeong <71077419+ye1ne@users.noreply.github.com>
2023-02-24 21:51:41 +0900

    [feat] JWTIntercepter 구현

---
+++ src/main/java/uyu/server/util/JwtInterceptor.java
@@ -0,0 +1,48 @@
+package uyu.server.util;
+
+import io.jsonwebtoken.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //클라이언트 요청에서 JWT 토큰 추출
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+        String token = authorizationHeader.substring(7);
+
+        // JWT 토큰 검증
+        try {
+            Claims claims = Jwts.parser().setSigningKey("secret".getBytes()).parseClaimsJws(token).getBody();
+            request.setAttribute("userId", claims.get("userId"));
+        } catch (JwtException ex) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if (ex != null) {
+            if (ex instanceof SignatureException) {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            } else if (ex instanceof ExpiredJwtException) {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            }
+        }
+    }
+}
+
+
